4d3bbfcfd26d1b01d0197f62a973b637410674ff
Qry <qryseymour@gmail.com>
2024-04-13 15:04:21 -0400

    implemented basic character and world

---
+++ prototyping/world.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class world : Node2D
+{
+    private Polygon2D polygon2D = null;
+    private CollisionPolygon2D collisionPolygon2D = null;
+    public override void _Ready()
+    {
+        // This creates a visible environment through the CP2D and the child P2D without needing to manually edit the P2D every time we want to update things.
+        collisionPolygon2D = GetNode<CollisionPolygon2D>("StaticBody2D/CollisionPolygon2D");
+        polygon2D = GetNode<Polygon2D>("StaticBody2D/CollisionPolygon2D/Polygon2D");
+        polygon2D.Polygon = collisionPolygon2D.Polygon;
+    }
+}
