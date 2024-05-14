7506586e90e00d5264e463772910506d504106ff
Qry <qryseymour@gmail.com>
2024-05-14 15:27:48 -0400

    prototype platform branch worked out

--- prototyping/PrototypePlatforms.cs
+++ prototyping/PrototypePlatforms.cs
@@ -1,16 +1,15 @@
 using Godot;
 using System;
-using System.Drawing;
 
-public partial class world : Node2D
+public partial class PrototypePlatforms : StaticBody2D
 {
-    private Polygon2D polygon2D = null;
+	private Polygon2D polygon2D = null;
     private CollisionPolygon2D collisionPolygon2D = null;
     public override void _Ready()
     {
         // This creates a visible environment through the CP2D and the child P2D without needing to manually edit the P2D every time we want to update things.
-        collisionPolygon2D = GetNode<CollisionPolygon2D>("StaticBody2D/CollisionPolygon2D");
-        polygon2D = GetNode<Polygon2D>("StaticBody2D/CollisionPolygon2D/Polygon2D");
+        collisionPolygon2D = GetNode<CollisionPolygon2D>("CollisionPolygon2D");
+        polygon2D = GetNode<Polygon2D>("CollisionPolygon2D/Polygon2D");
         polygon2D.Polygon = collisionPolygon2D.Polygon;
         RenderingServer.SetDefaultClearColor(Colors.Cyan);
     }
